bcc0b0e4b9aa544ecdc3289e65086fc25b659d96
Lorenzo Valgimigli <lorenzo.valgimigli@studio.unibo.it>
2019-06-06 19:41:19 +0200

    Fixed some bugs, add listing features

--- src/lexcex.py
+++ src/lexcex.py
@@ -42,17 +42,18 @@ remember wich flag you have to use in order to achieved a precise task.
 
 parser = argparse.ArgumentParser(prog="lexcex",
                                  description=description)
-parser.add_argument('command', type=str, action="store", required=False,
+parser.add_argument('--command', '-c', nargs=1, type=str, action="store",
+                    default=['none'],
                     help="Linux Command on wich you wish to add/remove/see examples")
-parser.add_argument('--list-commands', '-lc', action="store_true",
+parser.add_argument('--list', '-l', action="store_true",
                     help="List all commands with at least one examples")
 parser.add_argument('--verbosity', '-v', action="store_true")
 parser.add_argument('--add', '-a', action="store_true",
                     help="Add examples to selected command")
 
 parse = parser.parse_args()
-selected_command = parse.command
-listing = parse.listcommands
+selected_command = parse.command[0]
+listing = parse.list
 verbosity = parse.verbosity
 adding = parse.add 
 
@@ -65,8 +66,93 @@ if verbosity:
 
 def main():
     setUpDirs()
+    if listing:
+        listCommands()
+        exit()
     if adding:
         addExamples()
+        exit()
+    menu()
+
+
+def menu():
+    print('''
+ ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄       ▄
+▐░▌          ▐░░░░░░░░░░░▌▐░▌     ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌     ▐░▌
+▐░▌          ▐░█▀▀▀▀▀▀▀▀▀  ▐░▌   ▐░▌ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀  ▐░▌   ▐░▌
+▐░▌          ▐░▌            ▐░▌ ▐░▌  ▐░▌          ▐░▌            ▐░▌ ▐░▌
+▐░▌          ▐░█▄▄▄▄▄▄▄▄▄    ▐░▐░▌   ▐░▌          ▐░█▄▄▄▄▄▄▄▄▄    ▐░▐░▌
+▐░▌          ▐░░░░░░░░░░░▌    ▐░▌    ▐░▌          ▐░░░░░░░░░░░▌    ▐░▌
+▐░▌          ▐░█▀▀▀▀▀▀▀▀▀    ▐░▌░▌   ▐░▌          ▐░█▀▀▀▀▀▀▀▀▀    ▐░▌░▌
+▐░▌          ▐░▌            ▐░▌ ▐░▌  ▐░▌          ▐░▌            ▐░▌ ▐░▌
+▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄  ▐░▌   ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄  ▐░▌   ▐░▌
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌     ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌     ▐░▌
+ ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀       ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀       ▀
+          ''')
+    print('\n')
+    print('''
+██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
+██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
+██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
+██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
+╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
+ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+          ''')
+    print("Menu: \n")
+    print("1. Add examples")
+    print("2. View examples")
+    print("3. List all commands")
+    print("4. Exit")
+    choise = input('Do your choise: ')
+    if int(choise) not in range(1,5):
+        print("Just 1,2,3,4, allowed mate... What did you think?")
+        exit()
+    if int(choise) == 1:
+        askCommand()
+        addExamples()
+    if int(choise) == 3:
+        listCommands()
+    if int(choise) == 2:
+        askCommand()
+        printExamples()
+
+
+
+def printExamples():
+    example = (user_home.joinpath(lexcex_dir)
+               .joinpath(lexcex_examples_dir)
+               .joinpath(selected_command))
+    if example.exists():
+        fd = example.open('r')
+        for line in fd.readlines():
+            printExample(json.loads(line))
+
+
+def printExample(example):
+    print("#"*80+"\n")
+    print("Command: " + example[COMMAND])
+    print("Description: " + example[DESCR])
+    if len(example[FLAGS]) >= 1:
+        print("FLAGS: ")
+        for key, value in example[FLAGS].items():
+            print("Flag: {}  => {} ".format(key, value))
+
+
+def askCommand():
+    global selected_command
+    print("Add example section")
+    selected_command = input("Type the command you wish add example for: ")
+
+
+
+
+def listCommands():
+    commands_dir = user_home.joinpath(lexcex_dir).joinpath(lexcex_examples_dir)
+    commands = [c.name for c in commands_dir.iterdir() if c.is_file()]
+    print('Command found {}\n'.format(len(commands)))
+    for c in commands:
+        print(c)
+
 
 
 def addExamples():
@@ -95,9 +181,7 @@ def addExamples():
         json.dump(command_complete, Path(outfile).open('a'))
     else:
         json.dump(command_complete, Path(outfile).open('w'))
-
-
-
+    Path(outfile).open('a').write('\n')
 
 
 
