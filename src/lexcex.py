aed7916b0c39d0aaa418cc678c9903f9f9d723e4
Lorenzo Valgimigli <lorenzo.valgimigli@studio.unibo.it>
2019-06-08 19:01:28 +0200

    Add removing feature, improving code

--- src/lexcex.py
+++ src/lexcex.py
@@ -1,50 +1,43 @@
 #!/usr/bin/python3
 
 import argparse
-from pathlib import Path 
+from pathlib import Path
 import json
 
 
-
-
-########################## SETTING VARIABLES ##################################
+# ------------------------- SETTING VARIABLES ----------------------------
 user_home = Path.home()
 lexcex_dir = '.lexcex'
 lexcex_examples_dir = 'examples'
 
 
-
-
-########################## GLOBAL VARIBLES ##################################
-
+# ------------------------ GLOBAL VARIBLES -----------------------------
 selected_command = ''
 verbosity = False
 listing = False
-adding = False 
+adding = False
 
 
-
-
-########################## JSON KEY STRING #################################
-
+# -------------------------- JSON KEY STRING -------------------------
 COMMAND = 'command'
 FLAGS = 'flags'
 DESCR = 'description'
 
 
-################################## ARGUMENTS ###################################
-
+# ----------------------------- ARGUMENTS -----------------------------
 description = '''
 Linux commands EXtensible Collection of EXamples.
 This tool collects a lot of examples of Linux commands to make easy
 remember wich flag you have to use in order to achieved a precise task.
 '''
 
+
 parser = argparse.ArgumentParser(prog="lexcex",
                                  description=description)
 parser.add_argument('--command', '-c', nargs=1, type=str, action="store",
                     default=['none'],
-                    help="Linux Command on wich you wish to add/remove/see examples")
+                    help='''Linux Command on wich you wish
+                    to add/remove/see examples''')
 parser.add_argument('--list', '-l', action="store_true",
                     help="List all commands with at least one examples")
 parser.add_argument('--verbosity', '-v', action="store_true")
@@ -55,7 +48,7 @@ parse = parser.parse_args()
 selected_command = parse.command[0]
 listing = parse.list
 verbosity = parse.verbosity
-adding = parse.add 
+adding = parse.add
 
 if verbosity:
     print("[INFO] Command selected: " + str(selected_command))
@@ -93,8 +86,8 @@ def menu():
     print('''
 ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
 ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
-██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
-██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
+██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗
+██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝
 ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
  ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
           ''')
@@ -102,9 +95,10 @@ def menu():
     print("1. Add examples")
     print("2. View examples")
     print("3. List all commands")
-    print("4. Exit")
+    print("4. Delete Section")
+    print("5. Exit")
     choise = input('Do your choise: ')
-    if int(choise) not in range(1,5):
+    if int(choise) not in range(1, 6):
         print("Just 1,2,3,4, allowed mate... What did you think?")
         exit()
     if int(choise) == 1:
@@ -115,7 +109,56 @@ def menu():
     if int(choise) == 2:
         askCommand()
         printExamples()
+    if int(choise) == 4:
+        deleteMenu()
 
+
+def deleteMenu():
+    ok = True
+    choise = ''
+    while ok:
+        print("\n---- Delete Menu ---- ")
+        print("\n1. Delete All Commands")
+        print("2. Delete Single Command")
+        print("3. Delete Examples")
+        choise = input('do your choise: ')
+        if int(choise) not in range(1, 6):
+            print("Just 1,2,3,4, allowed mate... What did you think?")
+        else:
+            ok = False
+    if int(choise) == 1:
+        deleteAll()
+    if int(choise) == 2:
+        deleteOne()
+    if int(choise) == 3:
+        deleteExample()
+
+
+def deleteAll():
+    listCommands()
+    choise = input("\n Proceed?[Y/N]: ")
+    if 'y' in choise.lower():
+        directory = (user_home.joinpath(lexcex_dir)
+                     .joinpath(lexcex_examples_dir))
+        for cmd in directory.iterdir():
+            cmd.unlink()
+
+
+def deleteOne():
+    global selected_command
+    if selected_command == 'none':
+        askCommand()
+    choise = input("Remove {}?[Y/N]: ".format(selected_command))
+    if 'y' in choise.lower():
+        (user_home.joinpath(lexcex_dir)
+         .joinpath(lexcex_examples_dir)
+         .joinpath(selected_command)
+         .unlink())
+
+
+def deleteExample():
+    print("Currently not implemented... ;*(")
+    print("Delete it directly in json file man!")
 
 
 def printExamples():
@@ -140,28 +183,27 @@ def printExample(example):
 
 def askCommand():
     global selected_command
-    print("Add example section")
-    selected_command = input("Type the command you wish add example for: ")
+    print("Existing commands: ")
+    listCommands(' ')
+    selected_command = input("\n\n--->Type the command desired: ")
 
 
-
-
-def listCommands():
+def listCommands(endchar='\n'):
     commands_dir = user_home.joinpath(lexcex_dir).joinpath(lexcex_examples_dir)
     commands = [c.name for c in commands_dir.iterdir() if c.is_file()]
     print('Command found {}\n'.format(len(commands)))
     for c in commands:
-        print(c)
-
+        print(c, end=endchar)
+    print('\n-'*80)
 
 
 def addExamples():
     global selected_command
     com = input("Insert complete examples: ")
     desc = input("Insert a short description: ")
-    check = input("Would you like insert a short description for flags?[Y/N]:").lower()
+    check = input("Would you like insert a short description for flags?[Y/N]:")
     flags = dict()
-    if 'y' in check:
+    if 'y' in check.lower():
         while True:
             flag = input("Type the flag[STOP to end]: ")
             if 'STOP' in flag:
@@ -173,8 +215,8 @@ def addExamples():
     command_complete[DESCR] = desc
     command_complete[FLAGS] = flags
     outfile = str(user_home.joinpath(lexcex_dir)
-                 .joinpath(lexcex_examples_dir)
-                 .joinpath(selected_command))
+                  .joinpath(lexcex_examples_dir)
+                  .joinpath(selected_command))
     if verbosity:
         print("Examles will be saved in {} ".format(outfile))
     if Path(outfile).exists():
@@ -182,8 +224,6 @@ def addExamples():
     else:
         json.dump(command_complete, Path(outfile).open('w'))
     Path(outfile).open('a').write('\n')
-
-
 
 
 def setUpDirs():
@@ -197,16 +237,15 @@ def setUpDirs():
     if Path(directory).exists():
         if verbosity:
             print("[INFO] {} exists".format(directory))
-    else:     
+    else:
         if verbosity:
             print("[INFO] {} not exists".format(directory))
-        Path(directory).mkdir() 
+        Path(directory).mkdir()
         if verbosity:
             print("[INFO] {} Created".format(directory))
         directory_path.joinpath(lexcex_examples_dir).mkdir()
         if verbosity:
             print("[INFO] {} Created".format(lexcex_examples_dir))
-
 
 
 if __name__ == "__main__":
